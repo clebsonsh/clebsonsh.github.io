ad0f096e95bc324c134700dc73aa50d7784f9383
Clebson Moura <clebson.sh@gmail.com>
2022-07-07 17:53:01 -0300

    ejs para vite + react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.ejs"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
