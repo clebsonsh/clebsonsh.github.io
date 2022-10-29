8fe47d048954f4cfa4c0dd7cf28ba1fc5af0a2fb
Clebson Moura <clebson.sh@gmail.com>
2022-10-29 11:16:00 -0300

    React removed, using simple HTML, TailwindCSS and vite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html"],
   theme: {
     extend: {
       fontFamily: {
@@ -11,6 +11,14 @@ module.exports = {
       boxShadow: {
         btn: "4px 4px 0px 0px #000000",
       },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#1D1D1D",
+      white: "#F7F7F7",
+      accent: "#FACC15",
+      grey: "#798086",
     },
   },
   plugins: [],
